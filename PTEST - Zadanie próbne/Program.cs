7b3e3bd0abf48dd26f265f098e91ec32aa520304
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2022-04-17 00:24:16 +0200

    Dodaj pliki projektów.

---
+++ PTEST - Zadanie próbne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTEST___Zadanie_próbne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x+y);
+        }
+    }
+}
