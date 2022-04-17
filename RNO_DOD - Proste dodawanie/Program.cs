96e58a78c11dd70ef04c622bf6f37e5b3cc2a912
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2022-04-17 02:54:36 +0200

    RNO_DOD___Proste_dodawanie - błędnie napisane

---
+++ RNO_DOD - Proste dodawanie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNO_DOD___Proste_dodawanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t; // ilość prób
+            int n; //liczba liczb do dodania
+            int wynik;
+
+            t = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= t; i++)
+            {
+                wynik = 0;
+                n = int.Parse(Console.ReadLine());
+                for (int j = 1; j <= n; j++)
+                {
+                    int a = int.Parse(Console.ReadLine());
+                    wynik += a;
+                }
+                Console.WriteLine(wynik);
+            }
+        }
+        
+    }
+}
