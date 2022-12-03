8adad1d0920197f8bf867be1094792e8d2c18736
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2022-12-03 22:22:31 +0100

    Ukończone zadanie "Predkość średnia"

--- VSR - Predkość średnia/Program.cs
+++ VSR - Predkość średnia/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < a; i++)
+{
+    string[] bc = Console.ReadLine().Split();
+    double b = double.Parse(bc[0]);
+    double c = double.Parse(bc[1]);
+    Console.WriteLine((2 * b * c) / (b + c));
+}
