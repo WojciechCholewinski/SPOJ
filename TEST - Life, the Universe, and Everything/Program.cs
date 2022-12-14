1892db68f27f2018d3876a7c7b2faec1d2845200
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2022-12-15 00:03:11 +0100

    Ukończone zadanie "Life__the_Universe__and_Everything"

--- TEST - Life, the Universe, and Everything/Program.cs
+++ TEST - Life, the Universe, and Everything/Program.cs
@@ -10,6 +10,18 @@ namespace TEST___Life__the_Universe__and_Everything
     {
         static void Main(string[] args)
         {
+            List<string> lista = new List<string>();
+            string s;
+            do
+            {
+                s = Console.ReadLine();
+                lista.Add(s);
+            } while (s != "42");
+            lista.Remove(lista.Last());
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
